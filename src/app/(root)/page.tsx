cceda86059b73c84bf946c5ae45c03d1a72bb443
HYEONG\k2ker <kkerkk9@gmail.com>
2024-09-24 03:06:20 +0900

    안드로이드 테스트

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,3 +1,11 @@
 export default async function Main() {
-  return <main className="main">MAIN</main>;
+  return (
+    <main className="main">
+      <a href="intent://about#Intent;scheme=https;package=com.goldenwalnutapp;end">
+        Open App via Intent
+      </a>
+      <a href="goldenwalnut://">Open App</a>
+      <a href="goldenwalnut://permission">Open Permission</a>
+    </main>
+  );
 }
