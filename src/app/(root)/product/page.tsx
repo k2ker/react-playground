1c2f8a4ef6e258d6589b0d3fcfa3291184711c7b
HYEONG\k2ker <kkerkk9@gmail.com>
2025-05-22 23:18:17 +0900

    onlyHttp 테스트

--- src/app/(root)/product/page.tsx
+++ src/app/(root)/product/page.tsx
@@ -1,7 +1,23 @@
+"use client";
+
+import axios from "axios";
+
 export default function Product() {
+  const handleClickTest = () => {
+    axios
+      .post("https://04e4-211-179-11-20.ngrok-free.app/api/set-cookie", {
+        withCredentials: true,
+      })
+      .then((res) => {
+        console.log(res);
+      });
+  };
   return (
     <main className="main flex flex-grow flex-col gap-4 px-4  md:flex-row">
       <div>PRODUCT</div>
+      <div>
+        <button onClick={handleClickTest}>Open App</button>
+      </div>
     </main>
   );
 }
