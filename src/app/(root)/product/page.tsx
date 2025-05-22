605a71cfd69af716fa57781fe3a07251c70ba674
HYEONG\k2ker <kkerkk9@gmail.com>
2025-05-22 23:47:58 +0900

    바디에 추가

--- src/app/(root)/product/page.tsx
+++ src/app/(root)/product/page.tsx
@@ -5,18 +5,13 @@ import axios from "axios";
 export default function Product() {
   const handleClickTest = () => {
     axios
-      .post(
-        "https://04e4-211-179-11-20.ngrok-free.app/api/set-cookie",
-        {}, // ← 요청 바디 (필요 없으면 빈 객체)
-        {
-          withCredentials: true, // ✅ 올바른 위치
-        },
-      )
+      .post("https://04e4-211-179-11-20.ngrok-free.app/api/set-cookie", {
+        withCredentials: true,
+      })
       .then((res) => {
         console.log(res);
       });
   };
-
   return (
     <main className="main flex flex-grow flex-col gap-4 px-4  md:flex-row">
       <div>PRODUCT</div>
